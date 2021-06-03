ab92f7880f0945163bb1003c2585cb52f6d98a9e
Zack <zackgearhart65@gmail.com>
2021-06-02 23:59:04 -0500

    fixed rules

--- commands/command.js
+++ commands/command.js
@@ -9,8 +9,8 @@ module.exports = {
         .setDescription('This is a embed for the server rules')
         .addFields(
             {name: 'Rule 1', value: 'be nice'},
-            {name: 'Rule 1', value: 'follow goosehound'},
-            {name: 'Rule 1', value: 'love u'}
+            {name: 'Rule 2', value: 'follow goosehound'},
+            {name: 'Rule 3', value: 'love u'}
         )
         .setImage('https://f4.bcbits.com/img/0013974919_10.jpg')
         .setFooter('Check out the rules channel')
