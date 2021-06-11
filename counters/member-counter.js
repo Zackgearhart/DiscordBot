edef28ad94cb4b369ca126bdd8cc018a4a7befcb
Zack <zackgearhart65@gmail.com>
2021-06-10 23:30:07 -0500

    fixed member counter added procfile

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -1,10 +1,14 @@
 module.exports = async (client) =>{
-    const guild = client.guilds.cache.get('716316807912226837');
+    const guild = client.guilds.cache.get('849134665704669184');
+    const memberCount = guild.memberCount;
+    const channel = guild.channels.cache.get('852677971025068032');
+    channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
+    console.log('Updating Member Count...');
     setInterval(() =>{
         const memberCount = guild.memberCount;
         const channel = guild.channels.cache.get('852677971025068032');
         channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
-        console.log('Updating Member Count');
-    }, 600000);
+        console.log('Updating Member Count...');
+    }, 300000);
 }
  
