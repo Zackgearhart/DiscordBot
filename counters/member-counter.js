88e922240cb261b3b6cdcf6c045b70db051f8805
Zack <zackgearhart65@gmail.com>
2021-06-10 23:49:19 -0500

    test of gitignore

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -9,6 +9,6 @@ module.exports = async (client) =>{
         const channel = guild.channels.cache.get('852677971025068032');
         channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
         console.log('Updating Member Count...');
-    }, 300000);
+    }, 300001);
 }
  
